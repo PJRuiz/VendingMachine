e0b73b3d93e24f380d33bc789bed581e5d4060f2
Pedro Ruíz <pedro.ruiz.diaz@me.com>
2016-05-24 12:43:18 +0900

    Custom Controller

---
+++ VendingMachine/DepositController.swift
@@ -0,0 +1,23 @@
+//
+//  DepositController.swift
+//  VendingMachine
+//
+//  Created by Pedro Ruíz on 5/24/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class DepositController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
+	@IBAction func dismiss() {
+		dismissViewControllerAnimated(true, completion: nil)
+	}
+}
